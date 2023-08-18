b60a6990f4662f78971a9849b32611907eee2aa4
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-18 17:25:53 +0100

    Create pages files

---
+++ src/pages/Books.js
@@ -0,0 +1,11 @@
+import Books from '../Components/Books';
+import Form from '../Components/Form';
+
+const BooksPage = () => (
+  <div className="books_page">
+    <Books />
+    <Form />
+  </div>
+);
+
+export default BooksPage;
---
+++ src/pages/Categories.js
@@ -0,0 +1,3 @@
+const Categories = () => <h2>None</h2>;
+
+export default Categories;
