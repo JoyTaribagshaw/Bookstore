e5968b6d81e3cb7a38b18cb49a34a5dc80e3f3f3
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-21 17:42:51 +0100

    import slices correctly

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import booksSlice from '../books/booksSlice';
-import categoriesSlice from '../categories/categoriesSlice';
+import { categoriesSlice } from './categories/categoriesSlice';
+import { booksSlice } from './books/booksSlice';
 
 export default configureStore({
   reducer: {
