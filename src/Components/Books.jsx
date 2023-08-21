0a4a6831f25b39c4bf347d6d397e3f66b6ad583e
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-21 17:13:52 +0100

    Update Books.jsx component

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,22 +1,10 @@
 import Book from './Book';
 
-const Books = () => {
-  const books = [{
-    name: 'Geek Girl',
-    author: 'Holly Smalle',
-  }, {
-    name: 'Why you act the way you do',
-    author: 'Tim Lahaye',
-  }, {
-    name: 'Things fall apart',
-    author: 'Chinua Achebe',
-  }];
+const Books = ()=> {
 
-  return (
-    <div className="book">
-      {books.map((book) => <Book key={book.name} name={book.name} author={book.author} />)}
-    </div>
-  );
-};
+  return (<div className="books">
+      <Book name="title" author="author name"/>
+  </div>);
+}
 
 export default Books;
