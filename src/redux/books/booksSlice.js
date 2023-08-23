708e41ea0dff987213fefffe8fbf6c34fcbef16d
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-23 11:00:13 +0100

    Create initialState variable for my booksSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -3,16 +3,43 @@ import { createSlice } from '@reduxjs/toolkit';
 export const booksSlice = createSlice({
   name: 'books',
   initialState: {
-    value: [],
+    value: [
+      {
+        item_id: 'item1',
+        title: 'The Great Gatsby',
+        author: 'John Smith',
+        category: 'Fiction',
+      },
+      {
+        item_id: 'item2',
+        title: 'Anna Karenina',
+        author: 'Leo Tolstoy',
+        category: 'Fiction',
+      },
+      {
+        item_id: 'item3',
+        title: 'The Selfish Gene',
+        author: 'Richard Dawkins',
+        category: 'Nonfiction',
+      },
+    ],
   },
+
   reducers: {
     add: (state, action) => {
-      const { title, author, id } = action.payload;
-      state.value.push({ id, title, author });
+      const { title, author } = action.payload;
+      const newItem = {
+        item_id: `item${Math.floor(Math.random() * 10000)}`,
+        title,
+        author,
+        category: 'Fiction',
+      };
+      state.value.push(newItem);
     },
+
     remove: (state, action) => {
       const { id } = action.payload;
-      state.value = state.value.filter((book) => book.id !== id);
+      state.value = state.value.filter((book) => book.item_id !== id);
     },
   },
 });
