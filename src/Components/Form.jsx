6359ca6f8cbc6db78c123a492923626a6d8d4b88
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-23 11:01:57 +0100

    add buttons

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,10 +1,38 @@
-const Form = () => (
-  <form>
-    <header>ADD NEW BOOK</header>
-    <input type="text" placeholder="Book title" required />
-    <input type="text" placeholder="Book author" required />
-    <button type="submit">ADD BOOK</button>
-  </form>
-);
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { add } from '../redux/books/booksSlice';
+
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+  const handleClickAddBtn = (e) => {
+    e.preventDefault();
+    dispatch(add({ title, author }));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form>
+      <header>ADD NEW BOOK</header>
+      <input
+        type="text"
+        placeholder="Book title"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Book author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <button type="button" onClick={(e) => handleClickAddBtn(e)}>ADD BOOK</button>
+    </form>
+  );
+};
 
 export default Form;
