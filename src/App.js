c909fca5d18cdedae9dd233ca933414829b45217
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-23 11:02:16 +0100

    update app.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './index.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Categories from './pages/Categories';
 import Books from './pages/Books';
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Navbar />
 
     <Routes>
       <Route path="/" element={<Books />} />
       <Route path="/categories" element={<Categories />} />
     </Routes>
-  </>
+  </BrowserRouter>
 );
 
 export default App;
