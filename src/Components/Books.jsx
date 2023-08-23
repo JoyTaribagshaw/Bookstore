24af4038f46e0c2abb3350acf256e6acf5451e04
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-23 11:01:30 +0100

    display books

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,9 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
-const Books = () => (
-  <div className="books">
-    <Book name="title" author="author name" />
-  </div>
-);
+const Books = () => {
+  const books = useSelector((state) => state.books.value);
+
+  return (
+    <div className="books">
+      {books.map((book) => (
+        <Book
+          key={book.item_id}
+          id={book.item_id}
+          title={book.title}
+          author={book.author}
+          category={book.category}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Books;
